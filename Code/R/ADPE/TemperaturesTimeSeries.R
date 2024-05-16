f753bba5b9379dfab8223169b52310f931792c61
@buggemagn <buggemagn@ecs.vuw.ac.nz>
2024-05-16 19:38:41 +1200

    WeatherMain
    WeatherMain.R is the new main file
    Figures/PDFjpg/Weather contains plots from WeatherMain.R
    
    Variance is still uncertain

--- Code/R/ADPE/TemperaturesTimeSeries.R
+++ Code/R/ADPE/TemperaturesTimeSeries.R
@@ -10,170 +10,12 @@ library(prodlim)
 
 setwd(dirname(getActiveDocumentContext()$path))
 print(getwd())
-source("Bandt-Pompe.R")
+source("C:/Users/magnu/Desktop/skole/Vuw/BA/Research-Project/Code/R/ADPE/Bandt-Pompe.R")
 
-path = "StatOrdPattHxC/StatOrdPattHxC/R/"
-
-files = list.files(paste(getwd(),path,sep=""),"\\.R$")
-
-for (i in files){
-  source(paste("C:/Users/magnu/Desktop/skole/Vuw/BA/Research-Project/Code/R/",path,i,sep=""))
-}
 source("C:/Users/magnu/Desktop/skole/Vuw/BA/Research-Project/Code/R/ADPE/AsymptoticEntropyVariancesComparison.R")
 
 #####################################################################################
-
-## Read, organize, and select meteo data
-
-meteo = read.csv("./../../../Data/CSV/weatherBig.csv")
-
-
-
-set.seed(123)
-
-# Data max
-#index 16292 is 08/08/1992
-meteo_dublin_max = meteo[meteo$NAME == "DUBLIN PHOENIX PARK, EI",][16292:26297,"TMAX"]
-meteo_miami_max = meteo[meteo$NAME == "MIAMI INTERNATIONAL AIRPORT, FL US",][16292:26297,"TMAX"]
-meteo_edinburgh_max = meteo[meteo$NAME == "EDINBURGH ROYAL BOTANIC GARDE, UK",][11910:21915,"TMAX"]
-
-
-
-meteo_dublin_max = na.omit(meteo_dublin_max)
-meteo_edinburgh_max = na.omit(meteo_edinburgh_max)
-meteo_miami_max = na.omit(meteo_miami_max)
-
-path = "/StatOrdPattHxC/StatOrdPattHxC/R/"
-
-files = list.files(paste(getwd(),path,sep=""),"\\.R$")
-
-for (i in files){
-  source(paste(getwd(),path,i,sep=""))
-}
-source("C:/Users/magnu/Desktop/skole/Vuw/BA/Research-Project/Code/R/ADPE/AsymptoticEntropyVariancesComparison.R")
-
-
-dataPath = "./../../../Data/CSV/weather.csv"
-data = read.csv(dataPath)
-data = select(data, -STATION)
-
-data["NAME"] <- replace(data["NAME"], data["NAME"]=="MIAMI INTERNATIONAL AIRPORT, FL US","Miami")
-data["NAME"] <- replace(data["NAME"], data["NAME"]=="EDINBURGH ROYAL BOTANIC GARDE, UK","Edinburgh")
-data["NAME"] <- replace(data["NAME"], data["NAME"]=="DUBLIN PHOENIX PARK, EI","Dublin")
-
-#remove na values
-data <- na.omit(data)
-
-
-location = list("Miami","Edinburgh","Dublin")
-
-
-#for loop for counting number of patterns containing identical values for d=3
-#na.omit() needs to be activated above
-# count = c()
-# for (i in location){
-#   tmax = as.numeric(unlist(data[data["NAME"]==i,]["TMAX"]))
-#   n = length(tmax)
-#   counter=0
-#   for (j in 1:(n-2)){
-#     if(tmax[j]==tmax[j+1] | tmax[j]==tmax[j+2] | tmax[j+1]==tmax[j+2])
-#       counter = counter + 1
-#   }
-#   count = append(count,counter)
-# }
-# print(count)
-
-#add noise that only effect patterns with identical values. Both statcomp and pdc will always assign a observation containing
-#multiple identical values to the same pattern instead of a more evenly 50/50 divide between the two possible patterns.
-
-#commenting this line changes this entropy vastly especially for miami
-# set.seed(123)
-# data[,"TMAX"]=data[,"TMAX"]+runif(nrow(data),0,0.5)
-
-statcompOrdinalPattern = c()
-
-n2 = length(meteo_miami_max)
-rand = runif(n2,0,0.5)
-meteo_miami_max=meteo_miami_max+rand
-data[data["NAME"]=="Miami","TMAX"]=data[data["NAME"]=="Miami","TMAX"]+rand
-
-statcompOrdinalPattern = append(statcompOrdinalPattern,global_complexity(meteo_miami_max,ndemb=3)[1])
-
-
-n1 = length(meteo_edinburgh_max)
-rand = runif(n1,0,0.5)
-
-meteo_edinburgh_max=meteo_edinburgh_max+rand
-data[data["NAME"]=="Edinburgh","TMAX"]=data[data["NAME"]=="Edinburgh","TMAX"]+rand
-
-statcompOrdinalPattern = append(statcompOrdinalPattern,global_complexity(meteo_edinburgh_max,ndemb=3)[1])
-
-n = length(meteo_dublin_max)
-rand = runif(n,0,0.5)
-meteo_dublin_max=meteo_dublin_max+rand
-data[data["NAME"]=="Dublin","TMAX"]=data[data["NAME"]=="Dublin","TMAX"]+rand
-
-statcompOrdinalPattern = append(statcompOrdinalPattern,global_complexity(meteo_dublin_max,ndemb=3)[1])
-
-
-
-
-names(statcompOrdinalPattern) = c("miami","edinburgh","dublin")
-print(statcompOrdinalPattern)
-
-
-d = 3 
-#plot raw data
-#p <- ggplot(data=data,aes(DATE,TMAX,group=1))+geom_line()
-#p + facet_grid(rows=vars(NAME))
-
-
-
-
-#By using both na.omit() and adding tiny noise to all data points statcomp and pdc behaves in the same way, however if either
-#of the two preproccesing is lacking they start giving different entropies. The tiny noise is very important since 40% for miami and around 20-25%
-#Of the patterns for dublin and edinburgh have identical values. Statcomp and pdc assign these sets of observation to different patterns, which causes an error. 
-#NA values are also handled differently between them
-
-df = data.frame()
-d = 3
-
-switch = c(1,4,2,5,3,6)
-for (i in location){
-  tmax = as.numeric(unlist((data[data["NAME"]==i,"TMAX"])))
-  opd = ordinal_pattern_distribution(tmax,d)
-  n = sum(opd)
-  opd = opd[switch]*(1/n)
-  opd1 = OPprob(tmax,d)
-  entropyComplexity = global_complexity(opd=opd)[1:2]
-  a = sigma2q(tmax,d,"S")
-  oldVariance = v.n.q(n,d,opd) #function from ADPE/AsymptoticEntropyVariancesComparison.R
-  newVariance = sigma.n.q(n,d,opd) #function from ADPE/AsymptoticEntropyVariancesComparison.R
-  oldSd = oldVariance**(1/2) #standard deviation
-  newSd = newVariance**(1/2)#/(n**(1/2)) #standard deviation
-  z = qnorm(1-0.01/2) #z score
-  oldCI = z*oldSd #One side of the confidence interval
-  newCI = z*newSd #One side of the confidence interval
-  df = rbind(df,c(entropyComplexity,oldVariance,newVariance,2*oldCI,2*newCI))
-}
-df$location = location
-colnames(df) = c("entropy","complexity","oldVariance","newVariance","oldCILength","newCILength","location")
-
-print(df)
-
-
-# temp = formationPatternMagnus(meteo_edinburgh_max,D=3,tau=1)
-# n = sum(temp)
-# opd = temp/n
-# print(opd)
-# entropy = global_complexity(opd=opd)[1]
-# print(entropy)
-
-
-
-
-
-
+#just this function
 histD3 <- function(series){
 
   p.patterns = formationPattern(series, D = 3, tau = 1, 0)
@@ -199,26 +41,6 @@ histD3 <- function(series){
   index.rep = data.frame(i = index.rep)
   return(index.rep)
 }
-
-hist_dublin_max = histD3(meteo_dublin_max)
-hist_edinburgh_max = histD3(meteo_edinburgh_max)
-hist_miami_max = histD3(meteo_miami_max)
-
-
-hist_entropy = c()
-for (j in c(hist_dublin_max,hist_edinburgh_max,hist_miami_max)){
-  a = c(0,0,0,0,0,0)
-  for (i in unlist(j)){
-    a[i] = a[i]+1
-  }
-  n = sum(a)
-  a = a*(1/n)
-  entropy = global_complexity(opd=a)[1]
-  hist_entropy = append(hist_entropy,entropy)
-}
-names(hist_entropy) = c("dublin","edinburgh","miami")
-print(hist_entropy)
-
 
 
 
---
+++ Code/R/WeatherMain.R
@@ -0,0 +1,296 @@
+library(statcomp)
+library(ggplot2)
+library(ggthemes)
+library(scales)
+library(reshape2)
+library(rstudioapi)
+library(cowplot)
+library(gtools)
+library(prodlim)
+library(latex2exp)
+library(pdc)
+library(gridExtra)
+library(tidyverse)
+library(gtable)
+library(latexpdf)
+library(StatOrdPattHxC)
+
+setwd(dirname(getActiveDocumentContext()$path))
+
+#devtools::check("./StatOrdPattHxC/")
+#devtools::install("./StatOrdPattHxC/", reload = TRUE, build_vignettes = TRUE, build = TRUE)
+
+# }
+source("./ADPE/Bandt-Pompe.R")
+source("./ADPE/AsymptoticEntropyVariancesComparison.R")
+source("./ADPE/TemperaturesTimeSeries.R")
+
+dataPath = "./../../Data/CSV/weather.csv"
+
+runEntropyTable = T
+runConfidenceIntervalPlot = T
+runTest = F
+runNoise = T
+
+set.seed(123)
+#EntropyTable Code
+#####################################################################################
+if(runEntropyTable){
+  df = data.frame()
+  for(date in c(T,F)){ #date true means to set date to 14/08/1992, will only be done for noise==noNA==F
+    for(noise in c(T,F)){
+      for(noNA in c(T,F)){
+        if(date & (noise | noNA)){
+          next
+        }
+        data = read.csv(dataPath)
+        data = select(data, -STATION)
+  
+  
+        data["NAME"] <- replace(data["NAME"], data["NAME"]=="MIAMI INTERNATIONAL AIRPORT, FL US","Miami")
+        data["NAME"] <- replace(data["NAME"], data["NAME"]=="EDINBURGH ROYAL BOTANIC GARDE, UK","Edinburgh")
+        data["NAME"] <- replace(data["NAME"], data["NAME"]=="DUBLIN PHOENIX PARK, EI","Dublin")
+        
+        startDate = "1992-08-08"
+        if(date){
+          startDate = "19992-08-14"
+          datesToDelete = c("1992-08-08","1992-08-09","1992-08-10","1992-08-11","1992-08-12","1992-08-13")
+          data <- data[-which(data$DATE %in% datesToDelete),]
+        }
+        #remove na values
+        if(noNA){
+          data <- na.omit(data)
+        }
+  
+        location = c("Miami","Edinburgh","Dublin")
+  
+  
+        #for loop for counting number of patterns containing identical values for d=3
+        #na.omit() needs to be activated above
+        # count = c()
+        # for (i in location){
+        #   tmax = as.numeric(unlist(data[data["NAME"]==i,]["TMAX"]))
+        #   n = length(tmax)
+        #   counter=0
+        #   for (j in 1:(n-2)){
+        #     if(tmax[j]==tmax[j+1] | tmax[j]==tmax[j+2] | tmax[j+1]==tmax[j+2])
+        #       counter = counter + 1
+        #   }
+        #   count = append(count,counter)
+        # }
+        # print(count)
+  
+        #add noise that only effect patterns with identical values. Both statcomp and pdc will always assign a observation containing
+        #multiple identical values to the same pattern instead of a more evenly 50/50 divide between the two possible patterns.
+  
+        #commenting this line changes this entropy vastly especially for miami
+        if(noise){
+          data[,"TMAX"]=data[,"TMAX"]+runif(nrow(data),0,0.5)
+        }
+  
+  
+        d = 3
+        #plot raw data
+        #p <- ggplot(data=data,aes(DATE,TMAX,group=1))+geom_line()
+        #p + facet_grid(rows=vars(NAME))
+  
+  
+        #By using both na.omit() and adding tiny noise to all data points statcomp and pdc behaves in the same way, however if either
+        #of the two preproccesing is lacking they start giving different entropies. The tiny noise is very important since 40% for miami and around 20-25%
+        #Of the patterns for dublin and edinburgh have identical values. Statcomp and pdc assign these sets of observation to different patterns, which causes an error.
+        #NA values are also handled differently between them
+  
+  
+        #generate entropy tables
+        d = 3
+        entropyDf = data.frame()
+        switch = c(1,4,2,5,3,6) #statcomp has a different ordinal pattern order then article implementation
+        for (i in location){
+          tmax = as.numeric(unlist((data[data["NAME"]==i,"TMAX"])))
+          opd = ordinal_pattern_distribution(tmax,d) #statcomp
+          n = sum(opd)
+          opd = opd[switch]*(1/n)
+          e = HShannon(opd)
+  
+          opd1 = codebook(tmax,m=d) #pdc
+          e1 = HShannon(opd1)
+  
+  
+          hist = histD3(tmax) #hist implementation of odp
+          opd2 = c(0,0,0,0,0,0)
+          for (j in unlist(hist)){
+            opd2[j] = opd2[j]+1
+          }
+          n = sum(opd2)
+          opd2 = opd2*(1/n) #opd article implementation
+          e2= HShannon(opd2)
+  
+          e3= NA
+          if(noNA){
+            opd3 = OPprob(tmax,d) #StatOrdPattHxC newest implementation from article authors
+            e3 = HShannon(opd3)
+          }
+  
+          opd4 = formationPatternMagnus(tmax,D=d,tau=1) #my implementation with even split on ties
+          n = sum(opd4)
+          opd4 = opd4/n
+          e4 = HShannon(opd4)
+  
+  
+  
+          entropyDf = rbind(entropyDf,c(e,e1,e2,e3,e4))
+        }
+        colnames(entropyDf) = c("statcomp","pdc","article implementation","StatOrdPattHxC","my implementation")
+        entropyDf = round(entropyDf,digits=5)
+        entropyDf$location <- location
+        entropyDf$noiseAdded <- c(noise,"","")
+        entropyDf$naOmitted <-c(noNA,"","")
+        entropyDf$StartDate <- c(startDate,"","")
+        entropyDf = relocate(entropyDf,noiseAdded,naOmitted,StartDate,location)
+        df = rbind(df,entropyDf)
+      }
+    }
+  }
+  as.pdf(df,stem="entropyTable",dir="./../../Figures/PDFjpg/Weather/")
+}
+#####################################################################################
+
+
+#Test area
+if (runTest){
+  set.seed(1234567890, kind="Mersenne-Twister")
+  x <- rnorm(10000) # white noise
+  y <- rpois(5000, lambda=50) # smoothed with moving averages
+  # Asymptotic variances of their Shannon entropies
+  print(sigma2q(x, emb=4, ent="S"))
+  print(sigma2q(y, emb=4, ent="S"))
+  print(global_complexity(x,ndemb=3))
+}
+
+#Entropy plots where both noise and na.omit is activated. Confidence intervals around entropy
+#####################################################################################
+if(runConfidenceIntervalPlot){
+  set.seed(123)
+  
+  data = read.csv(dataPath)
+  data = select(data, -STATION)
+  
+  
+  data["NAME"] <- replace(data["NAME"], data["NAME"]=="MIAMI INTERNATIONAL AIRPORT, FL US","Miami")
+  data["NAME"] <- replace(data["NAME"], data["NAME"]=="EDINBURGH ROYAL BOTANIC GARDE, UK","Edinburgh")
+  data["NAME"] <- replace(data["NAME"], data["NAME"]=="DUBLIN PHOENIX PARK, EI","Dublin")
+  
+  data <- na.omit(data)
+  
+  data[,"TMAX"]=data[,"TMAX"]+runif(nrow(data),0,0.5)
+  
+  location = c("Miami","Edinburgh","Dublin")
+  
+  df = data.frame()
+  d=3
+  switch = c(1,4,2,5,3,6) #statcomp has a different ordinal pattern order then article implementation
+  for(i in location){
+    tmax = as.numeric(unlist((data[data["NAME"]==i,"TMAX"])))
+    opd = ordinal_pattern_distribution(tmax,ndemb=d)
+    n = sum(opd)
+    opd = opd[switch]*(1/n)
+    entropyComplexity = global_complexity(opd=opd)[1:2]
+    oldVariance = v.n.q(n,d,opd) #function from ADPE/AsymptoticEntropyVariancesComparison.R
+    sigmanq = sigma.n.q(n,d,opd)/(log(factorial(d))**2)
+    newVariance = sigma2q(tmax,d,"S")/(n+d-1) #function from ADPE/AsymptoticEntropyVariancesComparison.R
+    oldSd = oldVariance**(1/2) #standard deviation
+    newSd = newVariance**(1/2)#/(n**(1/2)) #standard deviation
+    z = qnorm(1-0.01/2) #z score
+    oldCI = z*oldSd #One side of the confidence interval
+    newCI = z*newSd #One side of the confidence interval
+    df = rbind(df,c(entropyComplexity,oldVariance,sigmanq,newVariance,2*oldCI,2*newCI))
+  }
+  df$location = location
+  colnames(df) = c("entropy","complexity","oldVariance","sigmanq","newVariance","oldCILength","newCILength","location")
+  
+  print(df)
+}
+
+
+#comparison of noise vs theoretical evenly split of ties
+#####################################################################################
+if(runNoise){
+  set.seed(123)
+  
+  iteration = 1000
+  data = read.csv(dataPath)
+  data = select(data, -STATION)
+  
+  
+  data["NAME"] <- replace(data["NAME"], data["NAME"]=="MIAMI INTERNATIONAL AIRPORT, FL US","Miami")
+  data["NAME"] <- replace(data["NAME"], data["NAME"]=="EDINBURGH ROYAL BOTANIC GARDE, UK","Edinburgh")
+  data["NAME"] <- replace(data["NAME"], data["NAME"]=="DUBLIN PHOENIX PARK, EI","Dublin")
+  
+  data <- na.omit(data)
+  
+  df = data.frame()
+  rows = nrow(data)
+  location = c("Miami","Edinburgh","Dublin")
+  d=3
+  
+  for (i in 1:iteration){
+    rand = runif(rows,0,0.5)
+    
+    data[,"TMAX"]=data[,"TMAX"]+rand
+    
+  
+    entropyList = c()
+    for(i in location){
+      tmax = as.numeric(unlist((data[data["NAME"]==i,"TMAX"])))
+      e = global_complexity(tmax,ndemb=d)[1]
+      entropyList = append(entropyList,e)
+    }
+    df = rbind(df,entropyList)
+    data[,"TMAX"]=data[,"TMAX"]-rand
+  }
+  colnames(df) = location
+  
+  te = c() #Theoretiacal Entropies
+  for(i in location){
+    tmax = as.numeric(unlist((data[data["NAME"]==i,"TMAX"])))
+    opd4 = formationPatternMagnus(tmax,D=d,tau=1) #my implementation with even split on ties
+    n = sum(opd4)
+    opd4 = opd4/n
+    te = append(te,HShannon(opd4))
+  }
+  
+  
+  meanM = round(mean(df[,1]),digits=7)
+  meanE = round(mean(df[,2]),digits=7)
+  meanD = round(mean(df[,3]),digits=7)
+  
+  teM = round(te[1],digits=7) #miami
+  teE = round(te[2],digits=7) #edinburgh
+  teD= round(te[3],digits=7) # dublin
+  
+  diffM = round(abs(mean(df[,1])-te[1]),digits=7)
+  diffE = round(abs(mean(df[,2])-te[2]),digits=7)
+  diffD = round(abs(mean(df[,3])-te[3]),digits=7)
+  
+  printDf = data.frame(matrix(c(meanM,meanE,meanD,teM,teE,teD,diffM,diffE,diffD),nrow=3,ncol=3,byrow=F))
+  colnames(printDf) = c("meanRandom","theoreticalSplit","diff")
+  printDf$location = location
+  printDf = relocate(printDf,location)
+  as.pdf(printDf,stem="random_vs_theoreticalSplit",dir="./../../Figures/PDFjpg/Weather/")
+  
+  pdf("./../../Figures/PDFjpg/Weather/noiseStochasticTheoretical.pdf")
+  par(mfrow=c(3,1))
+  plot(x=(1:iteration),y=sort(df[,"Miami"]),type="l",xlab="iteration",ylab="Entropy",main="Miami")
+  abline(h=te[1],v=iteration/2)
+  
+  plot(x=(1:iteration),y=sort(df[,"Edinburgh"]),type="l",xlab="iteration",ylab="Entropy",main="Edinburgh")
+  abline(h=te[2],v=iteration/2)
+  
+  plot(x=(1:iteration),y=sort(df[,"Dublin"]),type="l",xlab="iteration",ylab="Entropy",main="Dublin")
+  abline(h=te[3],v=iteration/2)
+  dev.off()
+  
+}
+
+
+
